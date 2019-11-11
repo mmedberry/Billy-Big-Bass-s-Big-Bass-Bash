78a57ff6e4f49da948b55d9e042806b1802193c9
mmedberry <maxrmedberry@gmail.com>
2019-11-11 15:45:45 -0600

    got hooksensor working with bobber

--- BBB/app/src/main/java/com/example/billybigbass/FishingActivity.java
+++ BBB/app/src/main/java/com/example/billybigbass/FishingActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
@@ -36,19 +37,28 @@ public class FishingActivity extends AppCompatActivity implements SensorUpdateCa
     private void catchFish() {
         Fish fish = new Fish(fishModel.getName(), fishModel.getLength(), fishModel.getWeight());
         database.eventDao().insertFish(fish);
-        Log.v("saving", "here");
         //Only save if high scores have been updated
         if (userDataModel.getHighScores().addFish(fish)) {
             userDataModel.saveUserData(this);
         }
+    }
 
+    public void getSetHookSensorStatus(SetHookSensor setHookSensor){
+        setHookSensor.start(System.currentTimeMillis()/1000);
     }
 
     @Override
-    public void update(float orientation) {
-        catchFish();
-        TextView text = findViewById(R.id.textView3);
-        text.setText("Success!");
+    public void update(boolean success) {
+        if (success){
+            catchFish();
+            TextView text = findViewById(R.id.textView3);
+            text.setText("Success!");
+        }
+        else{
+            TextView text = findViewById(R.id.textView3);
+            text.setText("Failure!");
+        }
+
     }
 
     public void cast(View view) {
@@ -57,7 +67,7 @@ public class FishingActivity extends AppCompatActivity implements SensorUpdateCa
         fishModel = new FishModel(fishNames[random.nextInt(4)]);
         Log.v("cast", "clicked");
         indicatorImage.setVisibility(View.VISIBLE);
-        FishingAwaitTimer fishingAwaitTimer = new FishingAwaitTimer(indicatorImage.getRotation(), this);
+        FishingAwaitTimer fishingAwaitTimer = new FishingAwaitTimer(indicatorImage.getRotation(), this, this);
         fishingAwaitTimer.execute();
     }
 
@@ -66,10 +76,12 @@ public class FishingActivity extends AppCompatActivity implements SensorUpdateCa
         int time;
         private Float orientation;
         private Context context;
+        private FishingActivity fishingActivity;
 
-        public FishingAwaitTimer( float orientation, Context context){
+        public FishingAwaitTimer( float orientation, Context context, FishingActivity fishingActivity){
             this.context = context;
             this.orientation = orientation;
+            this.fishingActivity = fishingActivity;
         }
         @Override
         protected String doInBackground(String... params) {
@@ -97,6 +109,8 @@ public class FishingActivity extends AppCompatActivity implements SensorUpdateCa
             MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.fish_hit);
             indicatorImage.animate().translationY(100.0f);
             mediaPlayer.start();
+            SetHookSensor hookSensor = new SetHookSensor(context, fishingActivity);
+            getSetHookSensorStatus(hookSensor);
         }
 
         @Override
